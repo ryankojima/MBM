cadc7c5056c167d13d3e4fef718d392799488477
Ryan Kojima <rk@interush.net>
2018-01-15 06:54:25 +0000

    Cleaning up files

--- models/session.js
+++
@@ -1,6 +0,0 @@
-var mongoose=require('mongoose');
-
-var sessionSchema= new mongoose.Schema({
-	token : { type: String, unique: true, required: true},
-});
-module.exports= mongoose.model('user',usersSchema);
--- models/user.js
+++
@@ -1,8 +0,0 @@
-var mongoose=require('mongoose');
-
-var userSchema= new mongoose.Schema({
-	email : { type: String, unique: true, required: true},
-	secret : { type: String, required : true }
-});
-
-module.exports= mongoose.model('user',usersSchema);
