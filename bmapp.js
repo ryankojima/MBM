e268e04a210fb0aaae1d7e7b5bd5e8753a8d5cc9
Ryan Kojima <rk@interush.net>
2018-01-15 07:13:39 +0000

    bmapp

--- bmapp.js
+++ bmapp.js
@@ -1,26 +1,71 @@
-var express= require('express');
+let express= require('express');
 
-var app = express();
+let app = express();
 
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 
-var fs = require('fs');
-var https = require('https');
+let config = require("config");
 
-var privateKey = fs.readFileSync('certs/private.key');
-var certificate = fs.readFileSync( 'certs/server.crt');
-//var ca = fs.readFileSync( 'encryption/intermediate.crt' );
+let fs = require('fs');
+let https = require('https');
+
+let users=require('./controllers/users');
+let login = require('./controllers/login');
+
+let privateKey = fs.readFileSync('certs/private.key');
+let certificate = fs.readFileSync( 'certs/server.crt');
+
+bodyParser=require('body-parser');
+
+app.use(bodyParser.json());;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/json'}));  
 
 
-app.get('/',function(req,res){
+
+let router=express.Router();
+
+
+app.get('/', (req,res) => {
 	console.log('getting a new request');
 	res.type('text/plain');
-	res.send('Hello SSL World');
+	res.send('Top design is under development.');
 });
+
+
+var mongoose = require('mongoose');
+let options = {
+	server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+	replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
+};
+mongoose.connect(config.get('DB_HOST'), options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+app.use('/api/v1',router);
+
+
+
+router.route("/login")
+	.post(login.authenticate);
+
+
+router.route("/user")
+	.get(users.getUsers)
+	.post(users.postUser);
+
+router.use("/user/:id",login.tokenRequired);
+
+router.route("/user/:id")
+	.get(users.IdMustBeAuthenticated,users.getUser)
+	.delete(users.IdMustBeAuthenticated,users.deleteUser)
+	.put(users.IdMustBeAuthenticated,users.updateUser);
 
 
 
 https.createServer({
 	key: privateKey,
 	cert: certificate
-}, app).listen(app.get('port'));
+}, app).listen(config.get('PORT'));
+
+module.exports = app; // for testing
